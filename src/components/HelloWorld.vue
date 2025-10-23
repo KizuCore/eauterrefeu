6b0604edf63864e99e61cdc477b7989077a5e80f
Adrien ROBERT <adri312678@outlook.fr>
2025-10-23 13:51:33 +0200

    Init play

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -34,6 +34,7 @@ const cols = ref(Array.from({ length: width },  (_, i) => i + 1))
 
 // Ensemble des cellules en feu
 const burning = ref(new Set())
+const burningAndHot = ref(new Set())
 
 // id stable pour la cellule (0..24)
 const cellId = (rIdx, cIdx) => rIdx * width + cIdx
@@ -41,22 +42,55 @@ const cellId = (rIdx, cIdx) => rIdx * width + cIdx
 // entier 0..(n-1)
 const rand = (n) => Math.floor(Math.random() * n)
 
+let isFinished = false;
+
+let intialFieldBurningID;
+let intialFieldBurningID2;
+
 // tire deux entiers distincts 0..(max-1)
 function pickTwoDistinct(max) {
-  const a = rand(max)
-  let b = rand(max)
-  while (b === a) b = rand(max)
-  return [a, b]
+  intialFieldBurningID = rand(max)
+  intialFieldBurningID2 = rand(max)
+  while (intialFieldBurningID2 === intialFieldBurningID) intialFieldBurningID2 = rand(max)
+  return [intialFieldBurningID, intialFieldBurningID2]
 }
 
 function initGame() {
-  const [a, b] = pickTwoDistinct(width * height) 
-  burning.value = new Set([a, b])
-  // console.log([...burning.value]) // debug
+  pickTwoDistinct(width * height) 
+  burning.value = new Set([intialFieldBurningID, intialFieldBurningID2])
+}
+
+function play(){
+  while(!isFinished){
+    setTimeout(() => {
+      playTurn();
+      console.log('Timeout atteint après 1 seconde !');
+    }, 1000);
+    isFinished = true;
+  }
+}
+
+function playTurn(){
+  let currentState = [];
+  burning.value.forEach(fieldBurn => {
+      let field = fieldBurn + height;
+      if(canBeBurned(field)){
+        currentState.push(field);
+      }
+    }
+  )
+  currentState.forEach(value => {
+     burning.value.add(value);
+  })
+}
+
+function canBeBurned(field){
+  return (field <= 25 && field >= 0 && !burning.value.has(field));
 }
 
 onMounted(() => {
   initGame()
+  play();
 })
 </script>
 
